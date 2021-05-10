3606130008976061433d8e179f6a0ae1cc238955
Falusi Sándor Viktor <h982683@stud.u-szeged.hu>
2021-05-10 18:13:43 +0200

    Delete adatok.sql

--- doc/adatok.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Felhasznalo VALUES('1','Mekk Elek','elekaprofi@mail.com','1351b2b5312','1234','Bukarest','Utcaaa','55','1');
-INSERT INTO Felhasznalo VALUES('2','Teszt Nber','nber@mail.com','1124b','4321','Szeged','Árokpart','11','0');
